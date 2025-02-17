9c9644a92bad148e18e905e0ce73ad143df647ff
Sagastein <ishimwe.sage.3@gmail.com>
2025-02-17 18:52:23 +0200

    add footer section to Events page with subscription form and background styling

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -9,6 +9,29 @@ const Events: React.FC = () => {
       <FinTechEvents />
       <UpcomingEvents />
       <PastEvents />
+      <footer
+        id="footer-sub"
+        className="flex gap-4 bg-[url('./community.jpg')] h-[300px]"
+      >
+        <aside className="flex flex-1 px-12 justify-center items-center">
+          <h1 className="text-white font-bold text-4xl">
+            Never Miss a FinTech Event Again!
+          </h1>
+        </aside>
+        <aside className="flex flex-col flex-1 justify-center ">
+          <h3 className="text-white text-2xl">News Letter</h3>
+          <div className="border w-10/12 border-secondary rounded-lg flex">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="p-2 w-full text-white focus:border-secondary bg-transparent border-none rounded-l-lg"
+            />
+            <button className="bg-secondary text-primary rounded-r-lg p-2 px-4 hover:bg-primary hover:text-secondary transition-colors duration-200">
+              Subscribe
+            </button>
+          </div>
+        </aside>
+      </footer>
     </main>
   );
 };
